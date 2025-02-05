5e34c7db2e74914c2ef44aa940315e0d6954332f
YSLennon <ysha0123@naver.com>
2025-02-05 12:18:44 +0900

    Comment Config

--- src/main/java/com/project/spboard/board/entity/Comment.java
+++ src/main/java/com/project/spboard/board/entity/Comment.java
@@ -1,8 +1,10 @@
 package com.project.spboard.board.entity;
 
+import com.project.spboard.core.entity.BaseEntity;
 import com.project.spboard.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,11 +16,17 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Board {
-    @Column(nullable = false)
-    private String title;
+public class Comment extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "board_no")
+    private Board boardNo;
     @Column(nullable = false)
     private String contents;
     @ManyToOne
+    @JoinColumn(name = "author")
     private Member author;
+    @ManyToOne
+    @JoinColumn(name = "parent_no")
+    private Comment parentNo;
 }
