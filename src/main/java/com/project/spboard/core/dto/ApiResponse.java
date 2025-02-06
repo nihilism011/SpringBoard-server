cf79c5b03fe2769e05af99a0d541d3b65347c4d1
YSLennon <ysha0123@naver.com>
2025-02-06 12:46:42 +0900

    feat: add ApiResponse

---
+++ src/main/java/com/project/spboard/core/dto/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.project.spboard.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+
+    public static <T> ResponseEntity<ApiResponse<T>> success (T data){
+        return ResponseEntity.ok(new ApiResponse<>("success", "요청이 성공했습니다.", data));
+    }
+    public static <T> ResponseEntity<ApiResponse<T>> success (String message, T data){
+        return ResponseEntity.ok(new ApiResponse<>("success", message, data));
+    }
+    public static <T> ResponseEntity<ApiResponse<T>> error (String message, HttpStatus status){
+        return ResponseEntity.status(status).body(new ApiResponse<>("error", message, null));
+    }
+}
